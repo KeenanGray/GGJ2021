fbe836b9e8e8ceb76fce4944761107792a902a40
Keenan Gray <keenangray@Keenans-MacBook-Pro-2.local>
2021-01-28 20:16:51 -0500

    initialize project

---
+++ Assets/Realistic VR Hands/AnimateHand.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.SpatialTracking;
+
+namespace Keenan_XR
+{
+    public class AnimateHand : MonoBehaviour
+    {
+        Animator animator;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            animator = GetComponent<Animator>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            print("TRACKED POSE :: " + GetComponent<TrackedPoseDriver>().poseSource);
+        }
+
+        public void UpdateHandAnimation(TrackedPoseDriver.TrackedPose pose, bool trigger, bool grip)
+        {
+             if (GetComponent<TrackedPoseDriver>().poseSource == pose)
+            {
+                animator.SetBool("Trigger Pressed", trigger);
+                animator.SetBool("Grip Pressed", grip);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/XR_Movement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Keenan_XR
+{
+    public class XR_Movement : MonoBehaviour
+    {
+
+        [SerializeField]
+        [Range(0, 10)]
+        float speed = 1;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            XR_Input.leftJoyAxisDelegate += movePlayer;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void movePlayer(Vector2 input)
+        {
+            print(input.x + ", " + input.y);
+            transform.Translate(transform.forward * input.x * speed);
+            transform.Translate(-transform.right * input.y * speed);
+        }
+    }
+
+}
